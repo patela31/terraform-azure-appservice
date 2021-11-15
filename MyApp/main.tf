464e57d0080fdd856da11b41161bd1a0cab906e6
Attila Patel <attila_patel@hotmail.com>
2021-11-15 16:46:17 +0000

    test

--- MyApp/main.tf
+++ MyApp/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "rg-app" {
-  name     = "atulrg-publicmodule"
+  name     = "atulrg-pubmod"
   location = "West Europe"
 }
 
@@ -7,8 +7,8 @@ module "webapp" {
   source  = "patela31/webapp/azurerm"
   version = "4.0.0"
   # insert the 3 required variables here
-  service_plan_name = "atulsp-publicmodule"
-  app_name = "atulapp-publicmodule"
+  service_plan_name = "atulsp-pubmod"
+  app_name = "atulapp-pubmod"
   location = azurerm_resource_group.rg-app.location
   resource_group_name = azurerm_resource_group.rg-app.name
 }
