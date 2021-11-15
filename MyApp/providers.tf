544112ac82f870d1b67741149705300ef341a23e
Attila Patel <attila_patel@hotmail.com>
2021-11-15 16:40:06 +0000

    test

--- MyApp/providers.tf
+++ MyApp/providers.tf
@@ -7,3 +7,9 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+provider "azuredevops" {
+  org_service_url = "https://dev.azure.com/apatel0999"
+  # personal_access_token = "nrhzilmy3c6xgfenuay2vayytnfothiyvqrwcxcmoqbbjkvm7irq"
+  personal_access_token = "jnuot46ocdb3vlswrb43hsp4bo5434qmodre3ifslgj6d4crzwsq"
+}
