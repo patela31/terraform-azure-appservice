3661ca83a80df5ea0be91b8cad572ade0362fc06
Attila Patel <attila_patel@hotmail.com>
2021-11-15 16:42:41 +0000

    removed pat

--- MyApp/providers.tf
+++ MyApp/providers.tf
@@ -8,8 +8,3 @@ provider "azurerm" {
   features {}
 }
 
-provider "azuredevops" {
-  org_service_url = "https://dev.azure.com/apatel0999"
-  # personal_access_token = "nrhzilmy3c6xgfenuay2vayytnfothiyvqrwcxcmoqbbjkvm7irq"
-  personal_access_token = "jnuot46ocdb3vlswrb43hsp4bo5434qmodre3ifslgj6d4crzwsq"
-}
